e8098e16445f5a28d488f11bf5176997fd88cf6f
lfernandes <lfernandes.iteris@alelo.com.br>
2025-01-15 11:41:48 -0300

    initial commit

---
+++ src/main/java/br/com/ldf/article/test/container/ArticleTestContainersApplication.java
@@ -0,0 +1,13 @@
+package br.com.ldf.article.test.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleTestContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleTestContainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ldf/article/test/container/ArticleTestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ldf.article.test.container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleTestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
